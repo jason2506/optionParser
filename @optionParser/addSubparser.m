8630d6fee411bd2291f2ed2e206b6a35c8ddb7b8
Chi-En Wu <jason2506.mail@gmail.com>
2013-11-14 21:56:43 +0800

    Set default `Prog` for subparsers

--- @optionParser/addSubparser.m
+++ @optionParser/addSubparser.m
@@ -7,12 +7,23 @@ elseif ~isempty(this.Subparsers) && ismember(name, {this.Subparsers.Name})
     error(['Conflicting parser name: ', name]);
 end
 
+% check parser instance
 if ~isa(parser, class(this))
     error('Parser must be an instance of optionParser');
 end
 
+if isempty(parser.Prog)
+    prog = this.Prog;
+    if isempty(prog)
+        prog = program_name;
+    end
+
+    parser.Prog = sprintf('%s %s', prog, name);
+end
+
 this.Subparsers(end + 1) = struct('Name', name, 'Parser', parser);
 
+% add subcommand option if it is not exist
 if ~ismember(this.SubcmdOptName, {this.Opts.Name})
     this = addOption(this, this.SubcmdOptName, [], 'ArgsNum', '1', ...
                      'Action', @handleSubcommand);
